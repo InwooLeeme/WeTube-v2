cd22a81868758d22b5bcda9ddb263f65289c3762
ItsmeLee <arsmagica90@gmail.com>
2020-11-04 21:15:29 +0900

    Installed middlewares
    Installed morgan,body-parser,helmet and cookie-parser.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require('express');
+
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = 4000;
 
-app.get('/',(req, res) => res.send('Home'));
+app.use(helmet(
+    {
+        contentSecurityPolicy: false,
+    }
+));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.listen(PORT,() => console.log(`Server is Running on : ${PORT}`));
