aefabb79e9f513ea1ed27a6767bf085ebec72d5c
ItsmeLee <arsmagica90@gmail.com>
2020-11-05 12:42:15 +0900

    Testing Home Routing
    Simple testing for home Routing.

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router.js";
 
 const app = express();
-
-const PORT = 4000;
 
 app.use(helmet(
     {
@@ -15,6 +14,11 @@ app.use(helmet(
     }
 ));
 app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.listen(PORT,() => console.log(`Server is Running on : ${PORT}`));
+app.use(userRouter);
+
+
+export default app;
